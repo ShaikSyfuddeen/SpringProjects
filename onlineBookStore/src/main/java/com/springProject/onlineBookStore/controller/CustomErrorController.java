f64f809be88f81055edbd28d6b97e8141b6a25f3
ShaikSyfuddeen <syfuddeen107@gmail.com>
2024-06-24 22:20:28 +0530

    Error Handling

---
+++ onlineBookStore/src/main/java/com/springProject/onlineBookStore/controller/CustomErrorController.java
@@ -0,0 +1,26 @@
+package com.springProject.onlineBookStore.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController{
+	
+	@RequestMapping("/error")
+	public String handleError(HttpServletRequest request, Model theModel) {
+		
+		Object errorCode = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		Object errorMessage = request.getAttribute(RequestDispatcher.ERROR_EXCEPTION_TYPE);
+		theModel.addAttribute("errorCode", errorCode.toString());
+		if(errorMessage != null) {
+			theModel.addAttribute("errorMessage", errorMessage.toString());
+		}
+		return "error";
+	}
+	
+}
