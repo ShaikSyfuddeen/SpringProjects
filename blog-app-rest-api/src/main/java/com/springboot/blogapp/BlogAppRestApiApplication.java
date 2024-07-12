1eb88fbe3927ef7f4ab0982b69de6062c70f2b3d
ShaikSyfuddeen <syfuddeen107@gmail.com>
2024-07-12 23:37:14 +0530

    setting up profiles

--- blog-app-rest-api/src/main/java/com/springboot/blogapp/BlogAppRestApiApplication.java
+++ blog-app-rest-api/src/main/java/com/springboot/blogapp/BlogAppRestApiApplication.java
@@ -1,9 +1,14 @@
 package com.springboot.blogapp;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import com.springboot.blogapp.entity.Role;
+import com.springboot.blogapp.repository.RoleRepository;
 
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -30,7 +35,7 @@ import io.swagger.v3.oas.annotations.info.License;
 							url = "https://github.com/ShaikSyfuddeen/SpringProjects/tree/master/blog-app-rest-api"
 						)
 		)
-public class BlogAppRestApiApplication {
+public class BlogAppRestApiApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogAppRestApiApplication.class, args);
@@ -39,6 +44,22 @@ public class BlogAppRestApiApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+		
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+		
 	}
 
 }
