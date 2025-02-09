c7f23a0a83bbf3926539e91869d0f5c322664b1e
ShaikSyfuddeen <syfuddeen107@gmail.com>
2025-02-09 21:36:06 +0530

    code refactoring

--- blog-app-rest-api/src/main/java/com/springboot/blogapp/payload/RegisterDTO.java
+++ blog-app-rest-api/src/main/java/com/springboot/blogapp/payload/RegisterDTO.java
@@ -1,16 +1,21 @@
 package com.springboot.blogapp.payload;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@AllArgsConstructor
 public class RegisterDTO {
 	
 	private String name;
 	private String username;
 	private String email;
 	private String password;
+	
+	public RegisterDTO() {
+	}
+	
+	public RegisterDTO(String name, String username, String email, String password) {
+		this.name = name;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
 	
 	public String getName() {
 		return name;
