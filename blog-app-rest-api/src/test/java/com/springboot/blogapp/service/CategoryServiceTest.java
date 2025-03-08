b1fc3146e42f2a46eca3d6f049e0abb4d33f96be
ShaikSyfuddeen <syfuddeen107@gmail.com>
2025-03-08 23:32:46 +0530

    Adding JUnit testcases for CategoryService

--- blog-app-rest-api/src/test/java/com/springboot/blogapp/service/CategoryServiceTest.java
+++ blog-app-rest-api/src/test/java/com/springboot/blogapp/service/CategoryServiceTest.java
@@ -1,7 +1,6 @@
 package com.springboot.blogapp.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -10,8 +9,12 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import com.springboot.blogapp.exception.ResourceNotFoundException;
+import jakarta.validation.constraints.Null;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,40 +49,92 @@ public class CategoryServiceTest {
 		CategoryDTO dto = new CategoryDTO(id, "Test Category-"+id, "Category-" + id + " for testing");
 		Category category = modelMapper.map(dto, Category.class);
         when(categoryRepository.save(Mockito.any(Category.class))).thenReturn(category);
-		assertNotEquals(null, categoryService.addCategory(dto));
+		assertEquals(CategoryDTO.class, categoryService.addCategory(dto).getClass());
 	}
 
 	@Test
-	public void getCategoryDTOTest() {
+	public void getCategoryDTOTest_returnsDTO() {
 		Category c = getSampleCategory(1l);
 		when(categoryRepository.findById(1l)).thenReturn(Optional.of(c));
-		assertNotEquals(null, categoryService.getCategoryDTO(1l));
+		assertEquals(CategoryDTO.class, categoryService.getCategoryDTO(1l).getClass());
+	}
+
+	@Test
+	public void getCategoryDTOTest_ThrowsResourceNotfoundException() {
+		when(categoryRepository.findById(1l)).thenThrow(new ResourceNotFoundException("Category", "id", 1l));
+		assertThrows(ResourceNotFoundException.class, () -> categoryService.getCategoryDTO(1l).getClass());
+	}
+
+	@Test
+	public void getCategoryDTOTest_ThrowsNullPointerException() {
+		when(categoryRepository.findById(1l)).thenThrow(new NullPointerException());
+		assertThrows(NullPointerException.class, () -> categoryService.getCategoryDTO(1l).getClass());
 	}
 	
 	@Test
-	public void getAllCategoriesTest() {
+	public void getAllCategoriesTest_returnsList() {
 		Category c1 = getSampleCategory(1l);
 		Category c2 = getSampleCategory(2l);
 		when(categoryRepository.findAll()).thenReturn(Stream.of(c1, c2).collect(Collectors.toList()));
 		assertEquals(2, categoryService.getAllCategories().size());
 	}
-	
+
 	@Test
-	public void updateCategoryTest() {
-		CategoryDTO dto = new CategoryDTO(1l, "Test Category", "Category for testing");
-		Category c = modelMapper.map(dto, Category.class);
+	public void getAllCategoriesTest_returnsNullPointerException() {
 		Category c1 = getSampleCategory(1l);
-		when(categoryRepository.findById(1l)).thenReturn(Optional.of(c1));
-		when(categoryRepository.save(c1)).thenReturn(c);
-		assertNotEquals(null, categoryService.updateCategory(dto, 1l));
+		Category c2 = getSampleCategory(2l);
+		when(categoryRepository.findAll()).thenReturn(null);
+		assertThrows(NullPointerException.class, ()-> categoryService.getAllCategories().size());
 	}
 	
 	@Test
-	public void deleteCategoryTest() {
+	public void updateCategoryTest_returnsDTO() {
+		CategoryDTO dto = new CategoryDTO(1L, "Test Category", "Category for testing");
+		Category c = modelMapper.map(dto, Category.class);
+		Category c1 = getSampleCategory(1L);
+		when(categoryRepository.findById(1L)).thenReturn(Optional.of(c1));
+		when(categoryRepository.save(c1)).thenReturn(c);
+		assertEquals(CategoryDTO.class, categoryService.updateCategory(dto, 1L).getClass());
+	}
+
+	@Test
+	public void updateCategoryTest_returnsResourceNotfoundException() {
+		CategoryDTO dto = new CategoryDTO(1l, "Test Category", "Category for testing");
+		Category c = modelMapper.map(dto, Category.class);
+		Category c1 = getSampleCategory(1l);
+		when(categoryRepository.findById(1l)).thenThrow(new ResourceNotFoundException("Category", "id", 1L));
+		when(categoryRepository.save(c1)).thenReturn(c);
+		assertThrows(ResourceNotFoundException.class, () -> categoryService.updateCategory(dto, 1l));
+	}
+
+	@Test
+	public void updateCategoryTest_returnsNullPointerException() {
+		CategoryDTO dto = new CategoryDTO(1l, "Test Category", "Category for testing");
+		Category c = modelMapper.map(dto, Category.class);
+		Category c1 = getSampleCategory(1l);
+		when(categoryRepository.findById(1l)).thenReturn(null);
+		when(categoryRepository.save(c1)).thenReturn(c);
+		assertThrows(NullPointerException.class, () -> categoryService.updateCategory(dto, 1l));
+	}
+	
+	@Test
+	public void deleteCategoryTest_returnsDTO() {
 		Category c = getSampleCategory(1l);
 		when(categoryRepository.findById(1l)).thenReturn(Optional.of(c));
 		categoryService.deleteCategory(1l);
 		verify(categoryRepository, times(1)).delete(c);
+	}
+
+	@Test
+	public void deleteCategoryTest_returnsResourceNotFoundException() {
+		Category c = getSampleCategory(1l);
+		when(categoryRepository.findById(1l)).thenThrow( new ResourceNotFoundException("Category", "id", 1L));
+	}
+
+	@Test
+	public void deleteCategoryTest_returnsNullPointerException() {
+		when(categoryRepository.findById(1l)).thenReturn(null);
+		assertThrows(NullPointerException.class, () -> categoryService.deleteCategory(1L));
 	}
 	
 }
